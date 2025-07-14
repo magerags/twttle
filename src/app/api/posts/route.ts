14f767e4553a43fb08baf3bcbdb3d66106328fb1
Tom Whittle <tomwhittle@hey.com>
2025-07-14 15:41:13 +0100

    testing runtime potential fix

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,6 +1,8 @@
 import { getBlogPostList } from "@/utils/file-helpers";
 import { NextResponse } from "next/server";
 
+export const runtime = "nodejs";
+
 export async function GET() {
   const posts = await getBlogPostList();
   return NextResponse.json(posts);
